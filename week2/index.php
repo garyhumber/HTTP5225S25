042b4af5089bbbb5cf75ebbcf3da16640abcfa59
Gary Bhanot <info@pixelr.io>
2025-05-14 16:34:56 -0400

    Week 2 PHP variables

---
+++ week2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Week 2</title>
+</head>
+<body>
+<!-- Learning Echo -->
+  <?php  
+  
+    echo "<h1>Hello World!</h1>";
+
+    echo "<p> My name is Gary Bhanot.
+          </p>"
+  
+  ?>
+
+<hr>
+
+<!-- Learning Variables -->
+
+<?php
+
+  $fname = "Gary";
+  $lname = "Bhanot";
+  echo "<h1>Hello $fname $lname</h1>";
+
+?>
+
+
+</body>
+</html>
